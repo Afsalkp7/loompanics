b808f8ae32f2b196c44c982ae354fdd4235034e8
Afsalkp7 <afsalkpmanu31@gmail.com>
2024-08-30 16:10:47 +0530

    Hero section

--- src/components/homePage/home/Home.jsx
+++ src/components/homePage/home/Home.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import './Home.css'
+import React from "react";
+import "./Home.css";
+import bookReadingKids from '../../../assets/bookReadingKids.png'
 function Home() {
   return (
     <>
+      <div className="hero">
+        <div className="heroText">
+          <span className="heroTextFirst">WE HAVE 1500+ BOOKS.</span><br />
+          <span className="heroTextSecond">Find your dream books <br /> from LOOMPANICS</span><br />
+          <span className="heroTextThird">
+            Find books, create membership and take your favourite<br/>
+            books.Carefully Buy after analyzing the needs of different geners.
+          </span><br></br>
+          <button className="heroTextBtn">Explore Books World</button>
+        </div>
+        <div className="HeroImage">
+          <img src={bookReadingKids} alt="Book reading kids" />
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
