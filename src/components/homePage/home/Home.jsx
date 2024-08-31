2f1f73feefe6ec39e4830f4b76062eafba74119e
Afsalkp7 <afsalkpmanu31@gmail.com>
2024-08-31 17:33:12 +0530

    user auth completed

--- src/components/homePage/home/Home.jsx
+++ src/components/homePage/home/Home.jsx
@@ -7,11 +7,11 @@ function Home() {
       <div className="hero">
         <div className="heroText">
           <span className="heroTextFirst">WE HAVE 1500+ BOOKS.</span><br />
-          <span className="heroTextSecond">Find your dream books <br /> from LOOMPANICS</span><br />
-          <span className="heroTextThird">
-            Find books, create membership and take your favourite<br/>
-            books.Carefully Buy after analyzing the needs of different geners.
-          </span><br></br>
+            <span className="heroTextSecond">Find your dream books <br /> from LOOMPANICS</span><br />
+            <span className="heroTextThird">
+              Find books, create membership and take your favourite<br/>
+              books.Carefully Buy after analyzing the needs of different geners.
+            </span><br />
           <button className="heroTextBtn">Explore Books World</button>
         </div>
         <div className="HeroImage">
