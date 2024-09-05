af9b3df8d6ca74e8d9647a1c5c25ca9a80ba666d
Afsalkp7 <afsalkpmanu31@gmail.com>
2024-09-05 17:34:34 +0530

    Home page category section completed

--- src/components/homePage/home/BrowseBooksSection.jsx
+++ src/components/homePage/home/BrowseBooksSection.jsx
@@ -7,7 +7,7 @@ function BrowseBooksSection() {
     <div className='browseBookSection'>
         <span className='browseHead'>Browse Our <span className='brouse500'>500+</span> Latest Books</span><br  />
         <span className='browseSubHead'>Discover and explore over 500 of latest books across various gernes to find your next read</span>
-        <button className='exploreButton'>Explore Now <MdOutlineRocketLaunch /></button>
+        <button className='exploreButton'><span className='exploreButtonText'> Explore Now <MdOutlineRocketLaunch /> </span></button>
     </div>
   )
 }
--- src/components/homePage/home/CategorySection.jsx
+++ src/components/homePage/home/CategorySection.jsx
@@ -1,14 +1,39 @@
 import React from 'react'
 import './categorySection.css'
+import { IoIosRocket } from "react-icons/io";
+import { FaArrowCircleRight } from "react-icons/fa";
+
+
+
 function CategorySection() {
+  const categories = [
+    { categoryName: "Drama", products: ["Product 1", "Product 2"] },
+    { categoryName: "Comedy", products: ["Product 1"] },
+    { categoryName: "Thriller", products: ["Product 1", "Product 2", "Product 3"] },
+    { categoryName: "Romance", products: ["Product 1", "Product 2"] },
+    { categoryName: "Science Fiction", products: ["Product 1", "Product 2", "Product 3", "Product 4"] },
+    { categoryName: "Horror", products: ["Product 1"] },
+    { categoryName: "Mystery", products: [] },
+    { categoryName: "Fantasy", products: ["Product 1", "Product 2"] }
+  ];
   return (
     <>
     <div className='categoryHead'>
         <span className='headMain'>Browse Books Categories</span> <br />
         <span className='headSub'>Choose from a wide range of categories to find the perfect books for you here.</span>
     </div>
-    <div className="categories">
-        
+    <div className="categories-container">
+      {categories.map((category, index) => (
+        <div key={index} className="category-square">
+          <div className="category-icon"><IoIosRocket /></div>
+          <div className="category-name">{category.categoryName}</div>
+          <div className="product-count">{category.products.length} products</div>
+        </div>
+      ))}
+      <div className="category_button-div">
+      <   button className='category-button'>Browse All Categories <FaArrowCircleRight/></button>
+      </div>
+      
     </div>
     </>
     
