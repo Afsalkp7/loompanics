f4b22c0dddccd7752f28ac3331562d8a2ec6f287
Afsalkp7 <afsalkpmanu31@gmail.com>
2024-09-06 11:54:11 +0530

    contact us page completed

--- src/components/contactus/ContactUs.jsx
+++ src/components/contactus/ContactUs.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
-import './contactus.css'
-import { MdLocationPin, MdOutlineCall, MdOutlineMailOutline } from 'react-icons/md'
-import { IoIosGlobe } from 'react-icons/io'
+import React from "react";
+import "./contactus.css";
+import {
+  MdLocationPin,
+  MdOutlineCall,
+  MdOutlineMailOutline,
+} from "react-icons/md";
+import { IoIosGlobe } from "react-icons/io";
 
-import logo from '../../assets/logo_without_bg.png'
+import logo from "../../assets/logo_without_bg.png";
 function ContactUs() {
   return (
     <>
@@ -15,24 +19,67 @@ function ContactUs() {
       </div>
       <div className="contact">
         <div className="contactLeft">
+          {/* Wrap content in a div to manage layering */}
+          <div className="contactLeftContent">
             <div className="loompanicsSection">
-                <img className="loompanicsImage" alt="logo image" src={logo}  />
-                <div className="loompanicsLinks">
-                    <span className="loompanicsName">Loompanics Online Books Store</span>
-                    <span className="loompanicsDetail">Since July 2017</span><br />
-                    <ul>
-                        <li><MdOutlineCall /><span></span></li>
-                        <li><MdOutlineMailOutline /></li>
-                        <li><IoIosGlobe /></li>
-                        <li><MdLocationPin /></li>
-                    </ul>
-                </div>
+              <img className="loompanicsImage" alt="logo image" src={logo} />
+              <div className="loompanicsLinks">
+                <span className="loompanicsName">
+                  Loompanics Online Books Store
+                </span>
+                <span className="loompanicsDetail">Since July 2017</span>
+                <br />
+                <ul>
+                  <li>
+                    <MdOutlineCall />
+                    <span></span>
+                  </li>
+                  <li>
+                    <MdOutlineMailOutline />
+                  </li>
+                  <li>
+                    <IoIosGlobe />
+                  </li>
+                  <li>
+                    <MdLocationPin />
+                  </li>
+                </ul>
+              </div>
             </div>
+          </div>
         </div>
-        <div className="contactRight">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugiat sunt eos culpa. Adipisci obcaecati ducimus quia commodi atque pariatur voluptatem tenetur natus, voluptatum aut reiciendis, ullam, iusto consequuntur laboriosam illo.</div>
+        <div className="contactRight">
+        <span className="addReviewSection">
+          <span className="aboutRightFirstHeading">Send Your Messages and feedback</span>
+          <form className="responsiveForm">
+      {/* Full width name field */}
+      <div className="formGroup fullWidth">
+        <input type="text" id="name" name="name" placeholder="Type your name" required />
+      </div>
+
+      {/* Email and Subject in one line */}
+      <div className="formGroup halfWidthContainer">
+        <div className="halfWidth">
+          <input type="email" id="email" name="email" placeholder="Enter your email" required />
+        </div>
+        <div className="halfWidth">
+          <input type="text" id="subject" name="subject" placeholder="Enter subject" required />
+        </div>
+      </div>
+
+      {/* Review Text Area */}
+      <div className="formGroup fullWidth">
+        <textarea id="review" name="review" rows="4" placeholder="Write your review..." required></textarea>
+      </div>
+
+      {/* Submit Button */}
+      <button type="submit" className="submitButton">Submit</button>
+    </form>
+          </span>
+        </div>
       </div>
     </>
-  )
+  );
 }
 
-export default ContactUs
+export default ContactUs;
--- src/components/layout/BottomNav.jsx
+++ src/components/layout/BottomNav.jsx
@@ -1,11 +1,36 @@
-// BottomNav.js
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom'; // useLocation will help identify the current path
 import './bottomNav.css';
 
 function BottomNav({ isOpenBottomNav, setIsOpenBottomNav, scrolled }) {
   const navigate = useNavigate();
-  const [activeItem, setActiveItem] = useState('HOME'); // State to keep track of the active item
+  const location = useLocation(); // Get the current path
+  const [activeItem, setActiveItem] = useState('');
+
+  // Effect to set the active item based on the current location
+  useEffect(() => {
+    const currentPath = location.pathname;
+
+    switch (currentPath) {
+      case '/':
+        setActiveItem('HOME');
+        break;
+      case '/books':
+        setActiveItem('BOOKS');
+        break;
+      case '/about':
+        setActiveItem('ABOUT US');
+        break;
+      case '/contact':
+        setActiveItem('CONTACT US');
+        break;
+      case '/membership':
+        setActiveItem('MEMBERSHIP');
+        break;
+      default:
+        setActiveItem('');
+    }
+  }, [location.pathname]); // Update whenever the path changes
 
   const handleNavigation = (item, path) => {
     setActiveItem(item);
