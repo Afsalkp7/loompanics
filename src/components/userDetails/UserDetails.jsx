a4e0a7b5a0650eef3583a3a2fda388562a556261
Afsalkp7 <afsalkpmanu31@gmail.com>
2024-09-08 11:28:41 +0530

    soket connection

--- src/components/userDetails/UserDetails.jsx
+++ src/components/userDetails/UserDetails.jsx
@@ -1,13 +1,8 @@
 import React from 'react'
-import UserChat from '../chat/Chat'
 
 const UserDetails = () => {
   return (
-    <>
     <div>UserDetails</div>
-    <UserChat />
-    </>
-    
   )
 }
 
