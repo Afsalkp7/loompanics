64fc603be0ba76ee109c8dca763412a398c91151
Afsalkp7 <afsalkpmanu31@gmail.com>
2024-09-08 11:42:04 +0530

    debug

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:4000/api/',
+  baseURL: 'https://loompanics-backend.vercel.app/api/',
 });
 
 API.interceptors.request.use(
